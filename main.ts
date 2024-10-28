f179f20d7e7499635ac12cbded64cab4a44ceb30
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-10-27 20:24:00 -0400

    spell check

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Joos
+ * Created on: Oct 2024
+ * This program plays cookie clicker
 */
 
-basic.showString('Hello, World!')
+let score = 0
+basic.showIcon(IconNames.Happy)
+//adds number to score and shows it
+input.onButtonPressed(Button.A, function() {
+    score = score + 1
+    basic.showNumber(score)
+})
+//resets score
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    score = 0
+    basic.showNumber(score)
+})
+
